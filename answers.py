1d98497d5403f678aa2eb4c4bf841fab6823c38d
Ezekiel Lokeru <simingor.lokeru@gmail.com>
2025-03-29 09:46:56 +0300

    Week 2 Assignment Completed

---
+++ answers.py
@@ -0,0 +1,21 @@
+#Question 1
+my_list = []
+
+#Question 2
+my_list.extend([10, 20, 30, 40])
+
+#Question 3
+my_list.insert(1, 15)
+
+#Question 4
+my_list.extend([50, 60, 70])
+
+#Question 5
+my_list.pop()
+
+#Question 6
+my_list.sort()
+
+#Question 7
+index_of_30 = my_list.index(30)
+print(index_of_30)
